4532f36c3bcfdb05a69d79a9730fb082ee638587
lsw71311 <lsw71311@naver.com>
2024-08-12 06:35:12 +0900

    Feat: Product 캐싱 처리

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/ProductApplication.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/ProductApplication.java
@@ -3,9 +3,13 @@ package com.sparta.msa_exam.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ProductApplication {
 
 	public static void main(String[] args) {
--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/ProductService.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/ProductService.java
@@ -1,6 +1,9 @@
 package com.sparta.msa_exam.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,17 +16,21 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     @Transactional
+    @CacheEvict(cacheNames = "productAllCache", allEntries = true)  //(새로운게 추가되었으니 전체 상품 캐시를 갱신)
+    @CachePut(cacheNames = "productCache", key = "#result.id")  //write through - 개별 상품 캐시 갱신
     public ProductResDto createProduct(ProductReqDto productReqDto) {
         Product product = Product.createProduct(productReqDto);
         Product savedProduct = productRepository.save(product);
         return toResponseDto(savedProduct);
     }
 
+    @Cacheable(cacheNames = "productAllCache", key = "#pageable.pageNumber + '-' + #pageable.pageSize")
     public Page<ProductResDto> getProducts(Pageable pageable) {
         Page<Product> products = productRepository.findAll(pageable);
         return products.map(this::toResponseDto);
     }
 
+    @Cacheable(cacheNames = "productCache", key = "args[0]")
     public ProductResDto getProduct(Long id) {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다. id=" + id));
