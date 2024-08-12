44cb6cd5a6d44427ee7c90adac0e40f5f66c8be7
lsw71311 <lsw71311@naver.com>
2024-08-12 16:19:28 +0900

    Fix: Order에 Serializable 직렬화 구현

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/OrderResDto.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/OrderResDto.java
@@ -2,6 +2,7 @@ package com.sparta.msa_exam.order;
 
 import lombok.*;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -10,7 +11,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class OrderResDto {
+public class OrderResDto implements Serializable {
     private String name;
     private List<Long> productIds;
 
